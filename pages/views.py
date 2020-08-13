e6361a6066e7f78a7b07752f574cb206d4634e74
umut <ozdemir.umut98@gmail.com>
2020-08-13 14:58:56 +0300

    add food and add exercise modals connected to the backend

--- pages/views.py
+++ pages/views.py
@@ -21,8 +21,14 @@ class HomeView(View):
         daily_person_of_current_user = current_user.fitness_person_user.dailyperson_set.filter(
             date_added__date=timezone.now().today().date())[0]
         food_set = Food.objects.all()
+        daily_exercise_program_of_current_user = daily_person_of_current_user.exerciseprogram_set.filter(
+            date_added__date=timezone.now().today().date())[0]
+        daily_diet_program = daily_person_of_current_user.dietprogram_set.filter(
+            date_added__date=timezone.now().today().date())[0]
         context = {'daily_person': daily_person_of_current_user,
-                   'food_set': food_set}
+                   'food_set': food_set,
+                   'daily_exercise_program': daily_exercise_program_of_current_user,
+                   'daily_diet_program': daily_diet_program}
         return render(request, self.template_name, context)
 
 
@@ -82,71 +88,25 @@ class DiaryView(View):
 
 
 @method_decorator(login_required, name='dispatch')
-class ChooseExerciseType(View):
-    template_name = 'pages/choose_exercise_type.html'
-    form_class = ExerciseTypeSelectionForm
-
-    def get(self, request, *args, **kwargs):
-        exercise_type_selection_form = self.form_class
-        context = {'form': exercise_type_selection_form}
-        return render(request, self.template_name, context)
-
-    def post(self, request, *args, **kwargs):
-        exercise_type_selection_form = self.form_class(request.POST)
-        exercise_type_choose = exercise_type_selection_form.data['exercise_type']
-        return redirect('add_exercise/' + exercise_type_choose)
-
-
-@method_decorator(login_required, name='dispatch')
 class AddExerciseView(View):
-    template_name = 'pages/add_exercise.html'
-    form_classes = {
-        'cardio_exercise_form': CardioExerciseForm,
-        'weight_exercise_form': WeightExerciseForm
-    }
-    exercise_type_choose = None
-
-    def get_object(self, queryset=None):
-        return queryset.get(exercise_type_choose=self.exercise_type_choose)
-
-    def get(self, request, *args, **kwargs):
-        # exercise_type_choose = 0 means user want to add cardio exercise.
-        # exercise_type_choose = 1 means user want to add weight exercise.
-        self.exercise_type_choose = self.kwargs['exercise_type_choose']
-        # Query exercises from database with respect to its exercise type.
-        exercises = Exercise.objects.filter(exercise_type=self.exercise_type_choose)
-        # list of exercise names from queried data.
-        exercise_name_list = [exercise.name for exercise in exercises]
-        # user want to add cardio exercise so context is cardio exercise form and exercises name list
-        # that contains cardio exercises.
-        if int(self.exercise_type_choose) == 0:
-            form = self.form_classes['cardio_exercise_form']
-            context = {'exercise_name_list': exercise_name_list, 'form': form}
-            return render(request, 'pages/add_exercise.html', context)
-        # user want to add weight exercise so context is weight exercise form and exercises name list
-        # that contains weight exercises.
-        elif int(self.exercise_type_choose) == 1:
-            form = self.form_classes['weight_exercise_form']
-            context = {'exercise_name_list': exercise_name_list, 'form': form}
-            return render(request, 'pages/add_exercise.html', context)
 
     def post(self, request, *args, **kwargs):
-        cardio_exercise_form = self.form_classes['cardio_exercise_form'](request.POST)
-        weight_exercise_form = self.form_classes['weight_exercise_form'](request.POST)
-        # if the user post cardio exercise form.
-        if cardio_exercise_form.is_valid():
+        exercise_type = int(request.POST.get('exercise_type', int))
+        # exercise_type -> 0 means Cardio Exercise
+        # exercise_type -> 1 means Weight Exercise
+        if exercise_type == 0:
             # current_user is fitness person of the current user.
             current_user = request.user.fitness_person_user
-            exercise_name = request.POST.get('exercise_selection')
-            duration = cardio_exercise_form.cleaned_data.get('duration')
+            exercise_id = request.POST.get('exercise_id', int)
+            duration = int(request.POST.get('duration', int))
+            daily_person_id = request.POST.get('daily_person_id', int)
+            daily_exercise_program_id = request.POST.get('daily_exercise_program_id', int)
             how_many_calorie_burn = duration * 6 * 3.5 * current_user.weight / 200
-            daily_person_of_current_user = current_user.dailyperson_set.filter(
-                date_added__date=timezone.now().today().date())[0]
-            daily_exercise_program_of_current_user = daily_person_of_current_user.exerciseprogram_set.filter(
-                date_added__date=timezone.now().today().date())[0]
+            daily_person_of_current_user = DailyPerson.objects.get(pk=daily_person_id)
+            daily_exercise_program_of_current_user = ExerciseProgram.objects.get(pk=daily_exercise_program_id)
             exercise_to_add = UserExercise.objects.create(
                 exercise_program=daily_exercise_program_of_current_user,
-                exercise=Exercise.objects.get(name=exercise_name),
+                exercise=Exercise.objects.get(pk=exercise_id),
                 duration=duration,
                 how_many_calorie_burn=how_many_calorie_burn)
             daily_person_of_current_user.save()
@@ -157,22 +117,24 @@ class AddExerciseView(View):
             calculations.calculate_daily_burned_calories(current_user)
             calculations.calculate_body_fat_percentage(current_user)
             current_user.save()
-            return redirect('diary')
-        # if the user post weight exercise form.
-        elif weight_exercise_form.is_valid():
+            data = {
+                'message': 'exercise added successfully'
+            }
+            return HttpResponse(json.dumps(data), content_type="application/json")
+        elif exercise_type == 1:
             # current_user is fitness person of the current user.
             current_user = request.user.fitness_person_user
-            exercise_name = request.POST.get('exercise_selection')
-            set_number = weight_exercise_form.cleaned_data.get('set_number')
-            rep_number = weight_exercise_form.cleaned_data.get('rep_number')
+            exercise_id = request.POST.get('exercise_id', int)
+            set_number = int(request.POST.get('set_number', int))
+            daily_person_id = request.POST.get('daily_person_id', int)
+            daily_exercise_program_id = request.POST.get('daily_exercise_program_id', int)
+            rep_number = int(request.POST.get('rep_number', int))
             how_many_calorie_burn = set_number * rep_number * 12
-            daily_person_of_current_user = current_user.dailyperson_set.filter(
-                date_added__date=timezone.now().today().date())[0]
-            daily_exercise_program_of_current_user = daily_person_of_current_user.exerciseprogram_set.filter(
-                date_added__date=timezone.now().today().date())[0]
+            daily_person_of_current_user = DailyPerson.objects.get(pk=daily_person_id)
+            daily_exercise_program_of_current_user = ExerciseProgram.objects.get(pk=daily_exercise_program_id)
             exercise_to_add = UserExercise.objects.create(
                 exercise_program=daily_exercise_program_of_current_user,
-                exercise=Exercise.objects.get(name=exercise_name),
+                exercise=Exercise.objects.get(pk=exercise_id),
                 set_number=set_number,
                 rep_number=rep_number,
                 how_many_calorie_burn=how_many_calorie_burn)
@@ -184,9 +146,11 @@ class AddExerciseView(View):
             calculations.calculate_daily_burned_calories(current_user)
             calculations.calculate_body_fat_percentage(current_user)
             current_user.save()
-            return redirect('diary')
-        else:
-            return redirect('home')
+            print('weight added')
+            data = {
+                'message': 'exercise added successfully'
+            }
+            return HttpResponse(json.dumps(data), content_type="application/json")
 
 
 def login(request):
@@ -229,42 +193,35 @@ class EditFitnessProfileView(View):
 
 @method_decorator(login_required, name='dispatch')
 class AddFoodView(View):
-    template_name = 'pages/add_food.html'
-    form_class = AddFoodForm
-
-    def get(self, request, *args, **kwargs):
-        food_set = Food.objects.all()
-        add_food_form = self.form_class
-        context = {'food_set': food_set, 'form': add_food_form}
-        return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        add_food_form = self.form_class(request.POST)
-        if add_food_form.is_valid():
-            # current_user is fitness person of the current user.
-            current_user = request.user.fitness_person_user
-            food_id = int(request.POST.get('food_selection'))
-            portion = add_food_form.cleaned_data.get('portion')
-            food_to_add = Food.objects.get(pk=food_id)
-            daily_person_of_current_user = current_user.dailyperson_set.filter(
-                date_added__date=timezone.now().today().date())[0]
-            daily_diet_program = daily_person_of_current_user.dietprogram_set.filter(
-                date_added__date=timezone.now().today().date())[0]
-            new_meal = Meal.objects.create(fitness_user=current_user, diet_program=daily_diet_program)
-            new_meal.foods.add(food_to_add)
-            new_meal.save()
-            daily_person_of_current_user.daily_protein_intake += (food_to_add.protein_amount * portion)
-            daily_person_of_current_user.daily_fat_intake += (food_to_add.fat_amount * portion)
-            daily_person_of_current_user.daily_carbohydrate_intake += (food_to_add.carbohydrate_amount * portion)
-            daily_person_of_current_user.daily_calorie_intakes += (food_to_add.calorie * portion)
-            daily_person_of_current_user.save()
-            daily_diet_program.save()
-            calculations.calculate_daily_required_calorie_intakes(current_user)
-            calculations.calculate_daily_macros(current_user)
-            calculations.calculate_body_mass_index(current_user)
-            calculations.calculate_daily_burned_calories(current_user)
-            current_user.save()
-        return redirect('diary')
+        # current_user is fitness person of the current user.
+        current_user = request.user.fitness_person_user
+        daily_person_id = request.POST.get('daily_person_id')
+        food_id = request.POST.get('food_id')
+        portion = int(request.POST.get('portion'))
+        food_to_add = Food.objects.get(pk=food_id)
+        daily_person_of_current_user = DailyPerson.objects.get(pk=daily_person_id)
+        daily_diet_program = daily_person_of_current_user.dietprogram_set.filter(
+            date_added__date=timezone.now().today().date())[0]
+        daily_diet_program.foods.add(food_to_add)
+        daily_person_of_current_user.daily_protein_intake += (food_to_add.protein_amount * portion)
+        daily_person_of_current_user.daily_fat_intake += (food_to_add.fat_amount * portion)
+        daily_person_of_current_user.daily_carbohydrate_intake += (food_to_add.carbohydrate_amount * portion)
+        daily_person_of_current_user.daily_calorie_intakes += (food_to_add.calorie * portion)
+        print(daily_person_of_current_user.daily_calorie_intakes)
+        daily_person_of_current_user.save()
+        daily_diet_program.save()
+        calculations.calculate_daily_required_calorie_intakes(current_user)
+        calculations.calculate_daily_macros(current_user)
+        calculations.calculate_body_mass_index(current_user)
+        calculations.calculate_daily_burned_calories(current_user)
+        current_user.save()
+        print('food added')
+        data = {
+            'message': 'exercise added successfully'
+        }
+        return HttpResponse(json.dumps(data), content_type="application/json")
 
 
 @method_decorator(login_required, name='dispatch')
